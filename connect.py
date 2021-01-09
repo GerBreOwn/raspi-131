6a8bab719419602b783ac7eecc1070cc04f16eac
gerbreown <gbrown@gerbreown.com>
2021-01-09 21:00:25 +0800

    Added 2 files and modified 1

---
+++ connect.py
@@ -0,0 +1,22 @@
+import psycopg2
+from config import config
+
+def connect():
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        print('Postgresql database version:')
+        cur.execute('select version()')
+        db_version = cur.fetchone()
+        print(db_version)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+if __name__ == '__main__':
+    connect()
--- main.py
+++ main.py
@@ -1,34 +1,71 @@
-def wind(speed):
-    """Define the wind speed routine"""
-    from gpiozero import Button
-    wind_speed_sensor = Button(5)
-    wind_count = 0
+import math
 
-    def spin():
-        global wind_count
-        wind_count =+ 1
-        return wind_count
+import statistics
+from datetime import time
 
-def bme280():
-    import bme280
-    import smbus2
-    from time import sleep
+from random import randrange
+from time import sleep
 
-    port = 1
-    address = 0x77
+import bme280
+import smbus2
+from gpiozero import Button
+
+# wind_count = 17
+radius_in = 3.0
+wind_interval = 5
+
+store_wind_speeds = []
+
+def rain():
+    bucket_size = 0.2794
+
+def bme280a():
+    port = 1    
+    address: int = 0x77
     bus = smbus2.SMBus(port)
     bme280.load.calibration_params(bus,address)
     while True
         bme280_data = bme280.sample(bus, address)
-        temperature = randrange(60, 110,0.3) # bme280_data.temperature
-        humidity = randrange(0, 100.0.3) # bme280_data.humidity
+        temperature = randrange(60, 110, 0.3) # bme280_data.temperature
+        humidity = randrange(0, 100, 0.3) # bme280_data.humidity
         pressure = randrange(60, 100, 0.3) # bme280_data.pressure
         sleep(1)
+
+def reset_wind():
+    global wind_count
+    wind_count = 0
+
+class Wind():
+    def spin(self):
+        global wind_count
+        wind_count += 1
+        return wind_count
+
+    def calc_speed(time_sec):
+        global wind_count
+        circfm_in = (2 * math.pi) * radius_in
+        rotations = wind_count / 2.0
+        dist_in = circfm_in * rotations
+        speed = dist_in / time_sec
+        return speed
+
+
+wind_speed_sensor = Button(5)
+wind_speed_sensor.when_pressed = Wind.spin()
+
+while True:
+    # global wind_count
+    wind_count = 0
+    sleep(wind_interval)
+    print(calc_speed(wind_interval), speed)
+
+
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    bme280()
-    wind(speed)
+    bme280a()
+    Wind()
+    rain()
